787a5757054be1970c9876ea19206c848d952d6f
Alex Rosca <roscaalex19@gmail.com>
2023-11-15 21:15:00 +0100

    Move the shared library download from make to cargo
    Shared libraries are automatically downloaded when they are missing
    or when Flutter is upgraded

--- embedder/build.rs
+++ embedder/build.rs
@@ -1,19 +1,110 @@
-use std::env;
-use std::path::PathBuf;
+use std::{env, io};
+use std::fmt::{Display, Formatter};
+use std::io::Write;
+use std::path::{Path, PathBuf};
+use std::process::Command;
 use std::str::FromStr;
 
+use lazy_static::lazy_static;
+
+const FLUTTER_ENGINE_LIBS_DIR: &str = "third_party/flutter_engine";
+const FLUTTER_ENGINE_LIB_NAME: &str = "libflutter_engine.so";
+const FLUTTER_ENGINE_LINK_NAME: &str = "flutter_engine";
+lazy_static! {
+    static ref FLUTTER_ENGINE_REVISION_FILE: String = format!("{FLUTTER_ENGINE_LIBS_DIR}/.flutter_engine_revision");
+}
+
 fn main() {
+    println!("cargo:rerun-if-changed={FLUTTER_ENGINE_LIBS_DIR}");
+
+    let flutter_engine_revision = exec("sh", &["-c", "flutter --version | grep Engine | awk '{print $NF}'"]);
+    let flutter_engine_build = match option_env!("FLUTTER_ENGINE_BUILD") {
+        Some(build) => FlutterEngineBuild::from_str(&build)
+            .expect("FLUTTER_ENGINE_BUILD must be one of debug, profile, release"),
+        None => FlutterEngineBuild::Debug,
+    };
+
+    if should_download_flutter_engine_library(&flutter_engine_revision, flutter_engine_build) {
+        download_flutter_engine_library(&flutter_engine_revision, flutter_engine_build);
+    }
     generate_embedder_bindings();
-    link_libflutter_engine();
+    link_libflutter_engine(flutter_engine_build);
+}
+
+fn should_download_flutter_engine_library(flutter_engine_revision: &str, flutter_engine_build: FlutterEngineBuild) -> bool {
+    if option_env!("SKIP_FLUTTER_ENGINE_DOWNLOAD").is_some() {
+        return false;
+    }
+    // Is the revision different? If so, Flutter was probably upgraded.
+    match std::fs::read_to_string(&*FLUTTER_ENGINE_REVISION_FILE) {
+        Ok(libs_revision) => {
+            if libs_revision != flutter_engine_revision {
+                return true;
+            }
+        },
+        Err(_) => return true,
+    };
+    // Does the shared library exist?
+    if Path::new(&format!("{FLUTTER_ENGINE_LIBS_DIR}/{flutter_engine_build}/{FLUTTER_ENGINE_LIB_NAME}")).exists() {
+        return false;
+    }
+    return true;
+}
+
+fn download_flutter_engine_library(flutter_engine_revision: &str, flutter_engine_build: FlutterEngineBuild) {
+    let arch = if cfg!(target_arch = "x86_64") {
+        "x64"
+    } else if cfg!(target_arch = "aarch64") {
+        "arm64"
+    } else {
+        panic!("Unsupported architecture");
+    };
+
+    // Download the archive.
+    let url = format!("https://github.com/sony/flutter-embedded-linux/releases/download/{flutter_engine_revision}/elinux-{arch}-{flutter_engine_build}.zip");
+    let bytes = download_from_url(&url)
+        .expect("Failed to download Flutter engine archive. Try downgrading Flutter.");
+    let mut archive = zip::ZipArchive::new(io::Cursor::new(bytes))
+        .expect("Not an archive");
+    let mut lib = archive.by_name(FLUTTER_ENGINE_LIB_NAME)
+        .expect(&format!("{FLUTTER_ENGINE_LIB_NAME} not found in archive"));
+
+    // Extract the shared library.
+    let lib_dir = format!("{FLUTTER_ENGINE_LIBS_DIR}/{flutter_engine_build}");
+    std::fs::create_dir_all(&lib_dir)
+        .expect("Failed to create directories");
+    let mut file = std::fs::File::create(format!("{lib_dir}/{FLUTTER_ENGINE_LIB_NAME}"))
+        .expect("Failed to create new file");
+    io::copy(&mut lib, &mut file)
+        .expect("Failed to copy Flutter engine library");
+
+    // Remember the revision.
+    let mut revision_file = std::fs::File::create(&*FLUTTER_ENGINE_REVISION_FILE)
+        .expect("Failed to create .flutter_engine_revision");
+    write!(revision_file, "{}", flutter_engine_revision)
+        .expect("Failed to write .flutter_engine_revision");
+}
+
+fn exec(cmd: &str, args: &[&str]) -> String {
+    let output = Command::new(cmd)
+        .args(args)
+        .output()
+        .expect("failed to execute cmd");
+
+    String::from_utf8(output.stdout).unwrap().trim().to_string()
+}
+
+fn download_from_url(url: &str) -> Result<bytes::Bytes, reqwest::Error> {
+    Ok(reqwest::blocking::get(url)?.error_for_status()?.bytes()?)
 }
 
 fn generate_embedder_bindings() {
-    let embedder_header_path = "third_party/flutter_engine/embedder.h";
+    let embedder_header_path = format!("{FLUTTER_ENGINE_LIBS_DIR}/embedder.h");
     println!("cargo:rerun-if-changed={embedder_header_path}");
 
     let bindings = bindgen::Builder::default()
         .header(embedder_header_path)
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .generate()
         .expect("Unable to generate bindings");
 
@@ -23,26 +114,13 @@ fn generate_embedder_bindings() {
         .expect("Couldn't write bindings!");
 }
 
-fn link_libflutter_engine() {
+fn link_libflutter_engine(flutter_engine_build: FlutterEngineBuild) {
     link_libgl();
 
-    let flutter_engine_config = match env::var("FLUTTER_ENGINE") {
-        Ok(config) => FlutterEngineConfig::from_str(&config).unwrap_or(FlutterEngineConfig::Debug),
-        Err(_) => FlutterEngineConfig::Debug,
-    };
-
-    let libflutter_engine_dirs = "third_party/flutter_engine";
-
-    let libflutter_engine_dir = match flutter_engine_config {
-        FlutterEngineConfig::Debug => format!("{libflutter_engine_dirs}/debug"),
-        FlutterEngineConfig::Profile => format!("{libflutter_engine_dirs}/profile"),
-        FlutterEngineConfig::Release => format!("{libflutter_engine_dirs}/release"),
-    };
-
-    println!("cargo:rerun-if-changed={libflutter_engine_dir}");
-
+    let libflutter_engine_dir = format!("{FLUTTER_ENGINE_LIBS_DIR}/{flutter_engine_build}");
+    println!("cargo:rerun-if-changed={libflutter_engine_dir}/{FLUTTER_ENGINE_LIB_NAME}");
     println!("cargo:rustc-link-search={libflutter_engine_dir}");
-    println!("cargo:rustc-link-lib=flutter_engine");
+    println!("cargo:rustc-link-lib={FLUTTER_ENGINE_LINK_NAME}");
 
     let rpath = if option_env!("BUNDLE").is_some() {
         "$ORIGIN/lib"
@@ -61,21 +139,32 @@ fn link_libgl() {
     println!("cargo:rustc-link-arg=-Wl,--no-as-needed,-lGL");
 }
 
-enum FlutterEngineConfig {
+#[derive(Clone, Copy)]
+enum FlutterEngineBuild {
     Debug,
     Profile,
     Release,
 }
 
-impl FromStr for FlutterEngineConfig {
+impl FromStr for FlutterEngineBuild {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s.to_lowercase().as_str() {
-            "debug" => Ok(FlutterEngineConfig::Debug),
-            "profile" => Ok(FlutterEngineConfig::Profile),
-            "release" => Ok(FlutterEngineConfig::Release),
+            "debug" => Ok(FlutterEngineBuild::Debug),
+            "profile" => Ok(FlutterEngineBuild::Profile),
+            "release" => Ok(FlutterEngineBuild::Release),
             _ => Err(()),
+        }
+    }
+}
+
+impl Display for FlutterEngineBuild {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            FlutterEngineBuild::Debug => write!(f, "debug"),
+            FlutterEngineBuild::Profile => write!(f, "profile"),
+            FlutterEngineBuild::Release => write!(f, "release"),
         }
     }
 }
