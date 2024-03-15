27500f5957477738614072751c131578d2829675
Alex Rosca <roscaalex19@gmail.com>
2024-03-16 00:57:18 +0100

    Handle window closing events for X11 surfaces

--- embedder/src/flutter_engine/platform_channel_callbacks.rs
+++ embedder/src/flutter_engine/platform_channel_callbacks.rs
@@ -362,17 +362,54 @@ pub fn close_window<BackendData: Backend + 'static>(
     let args = method_call.arguments().unwrap().clone();
     let payload: CloseWindowPayload = serde_json::from_value(args).unwrap();
 
-    match data.state.xdg_toplevels.get(&payload.surface_id).cloned() {
-        Some(toplevel) => {
-            toplevel.send_close();
-            result.success(None);
-        }
-        None => result.error(
+    let Some(wl_surface) = data.state.surfaces.get(&payload.surface_id).cloned() else {
+        result.error(
             "surface_doesnt_exist".to_string(),
             format!("Surface {} doesn't exist", payload.surface_id),
             None,
-        ),
+        );
+        return;
     };
+
+    let role = with_states(&wl_surface, |states| states.role);
+    match role {
+        Some(xdg::XDG_TOPLEVEL_ROLE) => {
+            let toplevel = data.state.xdg_toplevels.get(&payload.surface_id).cloned();
+
+            let Some(toplevel) = toplevel else {
+                result.error(
+                    "toplevel_doesnt_exist".to_string(),
+                    format!("Toplevel {} doesn't exist", payload.surface_id),
+                    None,
+                );
+                return;
+            };
+
+            toplevel.send_close();
+            result.success(None);
+        }
+        Some(xwm::X11_SURFACE_ROLE) => {
+            let Some(x11_surface) = data.state.x11_surface_per_wl_surface.get(&wl_surface) else {
+                result.error(
+                    "x11_surface_doesnt_exist".to_string(),
+                    format!("X11 Surface {} doesn't exist", payload.surface_id),
+                    None,
+                );
+                return;
+            };
+
+            let _ = x11_surface.close();
+            result.success(None);
+        }
+        _ => {
+            result.error(
+                "invalid_surface_role".to_string(),
+                format!("Surface {} has an invalid role", payload.surface_id),
+                None,
+            );
+            return;
+        }
+    }
 }
 
 pub fn get_monitor_layout<BackendData: Backend + 'static>(
