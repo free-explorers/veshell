f54f75cf8835f448fd5165981cf6a36bd21651e5
PapyElGringo <adrien@pesler.be>
2024-10-26 10:31:07 +0200

    record logs to file

--- embedder/src/main.rs
+++ embedder/src/main.rs
@@ -1,4 +1,10 @@
-use std::env;
+use std::{
+    env,
+    fs::{self, File},
+    io,
+    path::PathBuf,
+    time::SystemTime,
+};
 
 use backend::Backend;
 use log::debug;
@@ -11,6 +17,8 @@ use smithay::{
         with_surface_tree_downward, CompositorClientState, SurfaceAttributes, TraversalAction,
     },
 };
+use tracing_appender::rolling;
+use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
 use crate::flutter_engine::FlutterEngine;
 use crate::mouse_button_tracker::MouseButtonTracker;
@@ -30,11 +38,49 @@ mod wayland;
 mod xwayland;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    if let Ok(env_filter) = tracing_subscriber::EnvFilter::try_from_default_env() {
-        tracing_subscriber::fmt().with_env_filter(env_filter).init();
-    } else {
-        tracing_subscriber::fmt().init();
+    // Specify the absolute path for the logs directory
+    let mut log_dir = PathBuf::from("/var/log/veshell");
+
+    // Create the log directory if it doesn't exist
+    if !fs::create_dir_all(&log_dir).is_ok() {
+        let exe_path = env::current_exe().expect("Failed to get current executable path");
+        let exe_dir = exe_path
+            .parent()
+            .expect("Failed to get executable directory");
+
+        // Create the logs directory path
+        log_dir = exe_dir.join("logs");
     }
+    // Create a rolling file appender that rotates daily
+    let file_appender: rolling::RollingFileAppender = rolling::daily(log_dir, "veshell.log");
+
+    // Custom log formatting
+    let format = fmt::format()
+        .with_timer(fmt::time::ChronoLocal::new(String::from(
+            "%Y-%m-%d %H:%M:%S%.3f",
+        )))
+        .compact();
+
+    // Set up stdout layer with custom formatting
+    let stdout_layer = fmt::layer()
+        .event_format(format.clone())
+        .with_writer(std::io::stdout)
+        .with_ansi(true); // Colorful output for the terminal
+
+    // Set up file layer with custom formatting
+    let file_layer = fmt::layer()
+        .event_format(format)
+        .with_writer(file_appender)
+        .with_ansi(false); // No colors in log file
+
+    // Set up subscriber with both stdout and file layers
+    tracing_subscriber::registry()
+        .with(EnvFilter::from_default_env()) // Log level from RUST_LOG
+        .with(stdout_layer)
+        .with(file_layer)
+        .init();
+
+    debug!("Starting Veshell");
 
     // Fix XWayland crash when too many file descriptors are open.
     let _ = rlimit::increase_nofile_limit(u64::MAX);
