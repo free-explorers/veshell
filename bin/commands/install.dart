b3e9d3577fa09db17da39e314416c8636e5c9de9
urban-tree <192341479+urban-tree@users.noreply.github.com>
2024-12-20 16:09:59 +0100

    fix desktop file persmission

--- bin/commands/install.dart
+++ bin/commands/install.dart
@@ -142,6 +142,10 @@ X-GDM-SessionRegisters=true
       'sudo',
       ['cp', desktopFilePath, '/usr/share/wayland-sessions/'],
     );
+    await runProcess(
+      'sudo',
+      ['chmod', 'a+r', '/usr/share/wayland-sessions/veshell.desktop'],
+    );
   }
 
   logger.success('\nSession installation completed\n');
