b2890bb2545a8844ad71f0fe991167c5f013953e
Lars <61460660+larsb24@users.noreply.github.com>
2025-05-29 11:33:02 +0200

    Test CI with longer lines

--- src/shell/lib/workspace/widget/tileable/persistent_window/persistent_window.dart
+++ src/shell/lib/workspace/widget/tileable/persistent_window/persistent_window.dart
@@ -209,8 +209,7 @@ class PersistentWindowTileable extends Tileable {
               ...DisplayMode.values.map(
                 (e) => MenuItemButton(
                   leadingIcon: switch (e) {
-                    DisplayMode.maximized =>
-                      const Icon(MdiIcons.windowMaximize),
+                    DisplayMode.maximized => const Icon(MdiIcons.windowMaximize),
                     DisplayMode.fullscreen => const Icon(MdiIcons.fullscreen),
                     DisplayMode.game => const Icon(MdiIcons.controller),
                     DisplayMode.floating => SvgPicture.asset(
@@ -220,14 +219,10 @@ class PersistentWindowTileable extends Tileable {
                       ),
                   },
                   onPressed: () {
-                    ref
-                        .read(persistentWindowStateProvider(windowId).notifier)
-                        .setDisplayMode(e);
+                    ref.read(persistentWindowStateProvider(windowId).notifier).setDisplayMode(e);
                   },
                   style: MenuItemButton.styleFrom(
-                    foregroundColor: e == displayMode
-                        ? Theme.of(context).colorScheme.primary
-                        : null,
+                    foregroundColor: e == displayMode ? Theme.of(context).colorScheme.primary : null,
                   ),
                   child: Text(e.name),
                 ),
@@ -276,11 +271,9 @@ class WithSurfacesWidget extends HookConsumerWidget {
 
     final activateWindow = useCallback(
       (bool value) {
-        final metaWindowToActivate =
-            dialogWindowList.lastOrNull?.metaWindowId ?? window.metaWindowId;
+        final metaWindowToActivate = dialogWindowList.lastOrNull?.metaWindowId ?? window.metaWindowId;
         if (metaWindowToActivate != null) {
-          final metaWindow =
-              ref.read(metaWindowStateProvider(metaWindowToActivate));
+          final metaWindow = ref.read(metaWindowStateProvider(metaWindowToActivate));
 
           ref.read(waylandManagerProvider.notifier).request(
                 ActivateWindowRequest(
@@ -331,27 +324,18 @@ class WithSurfacesWidget extends HookConsumerWidget {
                   builder: (context) {
                     useEffect(
                       () {
-                        if (constraints
-                                .widthConstraints()
-                                .maxWidth
-                                .isInfinite ||
-                            constraints
-                                .heightConstraints()
-                                .maxHeight
-                                .isInfinite) {
+                        if (constraints.widthConstraints().maxWidth.isInfinite ||
+                            constraints.heightConstraints().maxHeight.isInfinite) {
                           return null;
                         }
-                        WidgetsBinding.instance
-                            .addPostFrameCallback((timeStamp) {
+                        WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
                           final geometry = ref.read(
-                            metaWindowStateProvider(window.metaWindowId!)
-                                .select((value) => value.geometry),
+                            metaWindowStateProvider(window.metaWindowId!).select((value) => value.geometry),
                           );
 
                           ref
                               .read(
-                                metaWindowStateProvider(window.metaWindowId!)
-                                    .notifier,
+                                metaWindowStateProvider(window.metaWindowId!).notifier,
                               )
                               .patch(
                                 UpdateGeometry(
@@ -399,8 +383,7 @@ class WithSurfacesWidget extends HookConsumerWidget {
       DisplayMode.floating => ContainerWithPositionnableChildren(
           children: [
             FloatableWindow(window: window),
-            for (final dialogWindow in dialogWindowList)
-              FloatableWindow(window: dialogWindow),
+            for (final dialogWindow in dialogWindowList) FloatableWindow(window: dialogWindow),
           ],
         ),
     };
