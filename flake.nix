09e293438aea455994db88586d0bb43d73ae221a
PapyElGringo <adrien@pesler.be>
2025-09-17 22:51:11 +0200

    test

--- flake.nix
+++ flake.nix
@@ -13,6 +13,10 @@
 
       # Get Flutter SDK
       flutter = pkgs.flutter332;
+
+      libPath = with pkgs; lib.makeLibraryPath [
+        # load external libraries that you need in your rust project here
+      ];
     in
     let
       lib = pkgs.lib;  
@@ -57,13 +61,13 @@
     {
       devShells.${system}.default = pkgs.mkShell {
         buildInputs = (with pkgs; [
-          rustc
-          cargo
+          clang
+          llvmPackages.bintools
+          rustup
           libGL
           libgbm
           libinput
           libxkbcommon
-          clang
           cmake
           pkg-config
           seatd
@@ -77,6 +81,31 @@
           jq
         ]) ++ [flutter];
 
+        # https://github.com/rust-lang/rust-bindgen#environment-variables
+        LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
+        shellHook = ''
+          export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
+          export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
+          '';
+        # Add precompiled library to rustc search path
+        RUSTFLAGS = (builtins.map (a: ''-L ${a}/lib'') [
+          # add libraries here (e.g. pkgs.libvmi)
+        ]);
+        LD_LIBRARY_PATH = libPath;
+        # Add glibc, clang, glib, and other headers to bindgen search path
+        BINDGEN_EXTRA_CLANG_ARGS =
+        # Includes normal include path
+        (builtins.map (a: ''-I"${a}/include"'') [
+          # add dev libraries here (e.g. pkgs.libvmi.dev)
+          pkgs.glibc.dev
+        ])
+        # Includes with special directory paths
+        ++ [
+          ''-I"${pkgs.llvmPackages_latest.libclang.lib}/lib/clang/${pkgs.llvmPackages_latest.libclang.version}/include"''
+          ''-I"${pkgs.glib.dev}/include/glib-2.0"''
+          ''-I${pkgs.glib.out}/lib/glib-2.0/include/''
+        ];
+        
         shellHook = ''
           export RUST_BACKTRACE=1
           export FLUTTER_PATH=${flutter}
