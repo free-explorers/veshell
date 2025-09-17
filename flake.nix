e548ff95c82d29e47add03dc05c27a100637ba43
PapyElGringo <adrien@pesler.be>
2025-09-17 22:59:46 +0200

    test

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = {nixpkgs, ...}:
+  outputs = {self, nixpkgs, ...}:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
